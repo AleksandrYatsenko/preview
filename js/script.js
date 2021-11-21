2f27530e2ee8b838ea67dab3e9cf7b73f78292f2
Alex <yatsikxvii@gmail.com>
2021-11-21 15:45:28 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// const obj = {
+//   name: 'John',
+//   age: 30,
+//   Married: false
+// };
+
+// console.log(obj['name']);
+
+// const arr = ['plumb.png', 'string', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(incr);
+console.log(decr);
+
+// f:\stadiing\udemy\js+react\preview
