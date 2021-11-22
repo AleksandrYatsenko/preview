ab507fd43dc9da67ca99f710da7c726abd2f4401
Alex <yatsikxvii@gmail.com>
2021-11-22 11:12:38 +0200

    less 015 practice if and for

--- js/script.js
+++ js/script.js
@@ -44,6 +44,40 @@
 //     break;
 // }
 
-for (let i = 1; i < 5; i++) {
-  console.log(i);
+// for (let i = 1; i < 5; i++) {
+//   console.log(i);
+// }
+
+const numberOfFilms = +prompt('Сколько фильмов вы уе посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+for (let i = 0; i < 2; i++){
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('Ваша оценка фильму?', '');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
 }
+
+if (personalMovieDB.count < 10) {
+  console.log('ochen malo');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('srednenko');
+} else if (personalMovieDB.count >=30) {
+  console.log('zadrot');
+} else {
+  console.log('err');
+}
+
+console.log(personalMovieDB);
